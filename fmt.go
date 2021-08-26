ed0f138c1ee843a65fd4a36cf9cb384a4e65fdd8
Diogo Cunha <71669420+namelew@users.noreply.github.com>
2021-08-26 14:53:36 -0300

    learning about the fmt functions

---
+++ fmt.go
@@ -0,0 +1,10 @@
+//package main
+
+import "fmt"
+
+func main()  {
+	fmt.Println("oi")
+	fmt.Printf("oi sem linha no final")
+	// também existem: Fprint's e Sprint's como no C. Funcionam da mesma forma
+
+}
